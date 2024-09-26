4dbac7eb2184b6cd1178190bd219c6a2df59e741
alasirimaya <alasirimaya@gmail.com>
2024-09-26 17:06:15 +0300

    Initial Commit

---
+++ GetHubGenral/GetHubGenralApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetHubGenralApp.swift
+//  GetHubGenral
+//
+//  Created by maya alasiri  on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GetHubGenralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
